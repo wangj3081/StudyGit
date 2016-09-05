fba64a13deffbfe3a88eb98dc7c5106a638cf187
wangjian <wangjian01@coreware.com>
2016-09-05 16:50:18 +0800

    地

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo{
+	public static void main(string[] args){
+		System.out.println("Hello World,Hello Git");
+		System.out.println("Hello World,Hello Git");
+		System.out.println("Hello World,Hello Git");
+	}
+}
